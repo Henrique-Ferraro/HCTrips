e0abedbe4972caf125ed87aa962a1ee481f7fb51
Henrique <hctferraro@gmail.com>
2022-08-27 20:58:21 -0300

    Alterações Projeto

--- hctrips/urls.py
+++ hctrips/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('receita', views.receita, name='receita')
+    path('trips', views.receita, name='trips')
 ]
--- hctrips/views.py
+++ hctrips/views.py
@@ -4,4 +4,4 @@ def index(request):
     return render(request,'index.html')
 
 def receita(request):
-    return render(request,'receita.html')
+    return render(request,'trips.html')
